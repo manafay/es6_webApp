afad7f1231e96d132c153e923a139fb0d5de4bf0
Nafay <manafay@gmail.com>
2016-11-06 22:52:58 +0530

    React from scratch

--- entry.js
+++ entry.js
@@ -1,1 +1,160 @@
-console.log('hello universe');
+const htmlAttributes = ['accept',
+'acceptCharset',
+'accessKey',
+'action',
+'allowFullScreen',
+'allowTransparency',
+'alt',
+'async',
+'autoComplete',
+'autoFocus',
+'autoPlay',
+'capture',
+'cellPadding',
+'cellSpacing',
+'challenge',
+'charSet',
+'checked',
+'cite',
+'classID',
+'className',
+'colSpan',
+'cols',
+'content',
+'contentEditable',
+'contextMenu',
+'controls',
+'coords',
+'crossOrigin',
+'data',
+'dateTime',
+'default',
+'defer',
+'dir',
+'disabled',
+'download',
+'draggable',
+'encType',
+'form',
+'formAction',
+'formEncType',
+'formMethod',
+'formNoValidate',
+'formTarget',
+'frameBorder',
+'headers',
+'height',
+'hidden',
+'high',
+'href',
+'hrefLang',
+'htmlFor',
+'httpEquiv',
+'icon',
+'id',
+'inputMode',
+'integrity',
+'is',
+'keyParams',
+'keyType',
+'kind',
+'label',
+'lang',
+'list',
+'loop',
+'low',
+'manifest',
+'marginHeight',
+'marginWidth',
+'max',
+'maxLength',
+'media',
+'mediaGroup',
+'method',
+'min',
+'minLength',
+'multiple',
+'muted',
+'name',
+'noValidate',
+'nonce',
+'open',
+'optimum',
+'pattern',
+'placeholder',
+'poster',
+'preload',
+'profile',
+'radioGroup',
+'readOnly',
+'rel',
+'required',
+'reversed',
+'role',
+'rowSpan',
+'rows',
+'sandbox',
+'scope',
+'scoped',
+'scrolling',
+'seamless',
+'selected',
+'shape',
+'size',
+'sizes',
+'span',
+'spellCheck',
+'src',
+'srcDoc',
+'srcLang',
+'srcSet',
+'start',
+'step',
+'style',
+'summary',
+'tabIndex',
+'target',
+'title',
+'type',
+'useMap',
+'value',
+'width',
+'wmode',
+'wrap',
+];
+
+const isAllowedAttribute = (attr) => {
+  const result = {};
+  if (attr) {
+    Object.keys(attr).forEach((key) => {
+      if (htmlAttributes.includes(key)) {
+        result.key = attr[key];
+      }
+    });
+  }
+  return result;
+};
+
+const transformAttribute = { className: 'class', htmlFor: 'for', onClick: 'on-click' };
+
+const transformAttrKey = (key) => {
+  if (transformAttribute.key !== undefined) {
+    return transformAttribute.key;
+  }
+  return key;
+};
+
+const getAttributes = (attr) =>
+Object.keys(attr).map((key) => `${transformAttrKey(key)}="${attr[key]}"`);
+
+const fetchChildren = (children) => children.map((elem) => `${elem} \n`);
+
+function createElement(tag, props, ...args) {
+  return `< ${tag}  ${getAttributes(isAllowedAttribute(props))}> ${fetchChildren(args)} </${tag}>`;
+}
+
+
+const child1 = createElement('li', null, 'Welcome to the world of React');
+const child2 = createElement('li', null, 'Build Resuable components');
+const className = { className: 'react', htmlFor: 'react-native' };
+console.log(createElement('h1', className, child1, child2));
